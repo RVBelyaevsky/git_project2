3ecd692e169c35847428346ed642f8be562413aa
RVBelyaevsky <rooooooma73@gmail.com>
2024-02-16 10:53:19 +0800

    добавляем функцию phrase_title и docstring к функциям

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 
 def up(word):
+    '''переводим в верхний регистр'''
     return word.upper()
+
+
+def phrase_title(phrase):
+    '''преобразует фразу, все слова с заглавной буквы'''
+    return phrase.title()
+
+
 
